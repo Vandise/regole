c20ed8ada975f45c7f7f527a82f0e424484ee4d3
Benjamin J Anderson <banderson@mashable.com>
2018-02-05 18:23:25 -0600

    extract tokens in array chunks

--- spec/assembler/lexer_spec.js
+++ spec/assembler/lexer_spec.js
@@ -1,5 +1,15 @@
 import Lexer from '../../src/assembler/lexer';
 
+const extractTokens = (lexer) => {
+  const results = [];
+  let token = lexer.nextToken();
+  while (Object.keys(token).length > 0) {
+    results.push(token.name);
+    token = lexer.nextToken();
+  }
+  return results;
+};
+
 describe('Lexer', () => {
 
   let lexer = null;
@@ -9,7 +19,9 @@ describe('Lexer', () => {
 
   it('can parse newline terminators', () => {
     lexer.setInput("\n");
-    expect(lexer.nextToken().name).toEqual('T_TERMINATOR');
+    expect(extractTokens(lexer)).toEqual(
+      ['T_TERMINATOR']
+    );
   });
 
 });
